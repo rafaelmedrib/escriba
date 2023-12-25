5315d1971bbe3064a164b3af25441df4eaff70fc
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-12-25 11:10:45 -0300

    add missing methods to FileManager

--- src/interfaces/IFileManager.ts
+++ src/interfaces/IFileManager.ts
@@ -1,5 +1,8 @@
+import { ReadStream } from "fs"
+
 export interface IFileManager {
   listDirectory(): string[]
   isDirectory(filePath: string): boolean
   isCsvFile(filePath: string): boolean
+  read(filePath: string): ReadStream
 }
